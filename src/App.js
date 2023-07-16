1b62cae2b398f8fc2c914210a12d464997c5407e
sahil <sahilchaubey25@gmail.com>
2023-07-16 19:03:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
+import Details from "./Components/Details";
+import Cart from "./Components/cart";
+import Default from "./Components/Default";
+import ProductList from "./Components/ProductList";
+import Modal from "./Components/Modal";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" Component={ProductList}></Route>
+
+        <Route exact path="/details" Component={Details}></Route>
+
+        <Route exact path="/cart" Component={Cart}></Route>
+
+        <Route path="*" Component={Default}></Route>
+      </Routes>
+
+      <Modal />
+    </React.Fragment>
   );
 }
 
